28a94d33bc7554de16ae8b9d994e7c9fe1d4c39e
tanhtruong <tanhtruong@gmail.com>
2025-11-12 20:45:32 +0100

    styles and package update

---
+++ App.styles.ts
@@ -0,0 +1,12 @@
+import {StyleSheet} from "react-native";
+import {colors} from "@lib/theme/colors";
+
+export default StyleSheet.create({
+    safeArea: {
+        flex: 1,
+        backgroundColor: colors.background,
+    },
+    container: {
+        flex: 1,
+    },
+});
--- App.tsx
+++ App.tsx
@@ -1,26 +1,27 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { AuthProvider } from "./src/context/AuthContext";
+import {StatusBar} from "expo-status-bar";
+import {initialWindowMetrics, SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {View} from "react-native";
+import {AuthProvider} from "./src/context/AuthContext";
 import RootNavigator from "./src/navigation/RootNavigator";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { colors } from "./src/lib/theme/colors";
-import DisclaimerBanner from "./src/components/DisclaimerBanner";
-import { View } from "react-native";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import styles from "./App.styles";
 
 const queryClient = new QueryClient();
+
 export default function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <StatusBar style="light" />
-        <SafeAreaView style={{ flex: 1, backgroundColor: colors.bg }}>
-          <DisclaimerBanner />
-          <View style={{ flex: 1 }}>
-            <RootNavigator />
-          </View>
-        </SafeAreaView>
-      </AuthProvider>
-    </QueryClientProvider>
-  );
+    return (
+        <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+            <QueryClientProvider client={queryClient}>
+                <AuthProvider>
+                    <StatusBar style="light"/>
+                    <SafeAreaView style={styles.safeArea}>
+                        <View style={styles.container}>
+                            <RootNavigator/>
+                        </View>
+                    </SafeAreaView>
+                </AuthProvider>
+            </QueryClientProvider>
+        </SafeAreaProvider>
+    );
 }
